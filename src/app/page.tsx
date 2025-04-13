d08665295a7e55a7bfd2581640360ca2818b9524
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-04-13 19:41:47 +0500

    fix: Page animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,17 @@ const Homepage = () => {
       className="h-full"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
-      transition={{ duration: 1 }}>
-      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      transition={{ duration: 1 }}
+    >
+      <div className="flex h-full flex-col px-4 sm:px-8 md:px-12 lg:flex-row lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 relative">
+        <div className="relative h-1/2 lg:h-full lg:w-1/2">
           <Image src="/hero.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
+        <div className="flex h-1/2 flex-col items-center justify-center gap-8 lg:h-full lg:w-1/2">
           {/* TITLE */}
-          <h1 className="text-4xl md:text-6xl font-bold">
+          <h1 className="text-4xl font-bold md:text-6xl">
             Crafting Digital Experiences, Designing Tomorrow.
           </h1>
           {/* DESC */}
@@ -29,11 +30,11 @@ const Homepage = () => {
             commitment to excellence.
           </p>
           {/* BUTTONS */}
-          <div className="w-full flex gap-4">
-            <button className="p-4 rounded-lg ring-1 ring-black bg-black text-white">
+          <div className="flex w-full gap-4">
+            <button className="rounded-lg bg-black p-4 text-white ring-1 ring-black">
               View My Work
             </button>
-            <button className="p-4 rounded-lg ring-1 ring-black">
+            <button className="rounded-lg p-4 ring-1 ring-black">
               Contact Me
             </button>
           </div>
--- src/components/PageChangeTransition.tsx
+++ src/components/PageChangeTransition.tsx
@@ -25,25 +25,27 @@ export default function PageChangeTransition({ children }: ChildrenProp) {
     <AnimatePresence mode="wait">
       <main
         key={pathName}
-        className="w-screen h-screen bg-gradient-to-b from-primary/10 to-secondary/15">
+        className="from-primary/10 to-secondary/15 h-screen w-screen bg-gradient-to-b"
+      >
         <motion.div
-          className="h-screen w-screen fixed bg-black rounded-b-[100px] z-40"
+          className="fixed z-40 h-screen w-screen rounded-b-[100px] bg-amber-50"
           animate={{ height: "0vh" }}
           exit={{ height: "140vh" }}
           transition={{ duration: 0.5, ease: "easeOut" }}
         />
 
         <motion.div
-          className={`fixed m-auto top-0 bottom-0 left-0 right-0 text-primary text-8xl cursor-default z-50 w-fit h-fit ${FontFingerPaint.className}`}
+          className={`text-primary fixed top-0 right-0 bottom-0 left-0 z-50 m-auto h-fit w-fit cursor-default text-8xl ${FontFingerPaint.className}`}
           initial={{ opacity: 1 }}
           animate={{ opacity: 0 }}
           exit={{ opacity: 0 }}
-          transition={{ duration: 0.8, ease: "easeOut" }}>
+          transition={{ duration: 0.8, ease: "easeOut" }}
+        >
           {transitionLabel}
         </motion.div>
 
         <motion.div
-          className="h-screen w-screen fixed bg-black rounded-t-[100px] bottom-0 z-30"
+          className="fixed bottom-0 z-30 h-screen w-screen rounded-t-[100px] bg-amber-50"
           initial={{ height: "140vh" }}
           animate={{ height: "0vh", transition: { delay: 0.5 } }}
         />
