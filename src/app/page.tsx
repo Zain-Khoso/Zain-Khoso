caec397ff90005ef45ad772b5e14a176094bf085
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-04-13 20:48:25 +0500

    update: Hero Image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,24 @@
 "use client";
 
-import Image from "next/image";
+// Lib Imports.
 import { motion } from "motion/react";
 
-const Homepage = () => {
+// Components.
+import HeroImage from "@/components/HeroImage";
+import HeroContext from "@/components/HeroContext";
+
+// Landing page of the portfolio.
+export default function LandingPage() {
   return (
-    <motion.div
-      className="h-full"
+    <motion.main
+      className="flex h-full min-h-fit flex-col overflow-hidden px-4 sm:px-8 md:px-12 lg:flex-row lg:px-20 xl:px-48"
       initial={{ y: "-200vh" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-      <div className="flex h-full flex-col px-4 sm:px-8 md:px-12 lg:flex-row lg:px-20 xl:px-48">
-        {/* IMAGE CONTAINER */}
-        <div className="relative h-1/2 lg:h-full lg:w-1/2">
-          <Image src="/hero.png" alt="" fill className="object-contain" />
-        </div>
-        {/* TEXT CONTAINER */}
-        <div className="flex h-1/2 flex-col items-center justify-center gap-8 lg:h-full lg:w-1/2">
-          {/* TITLE */}
-          <h1 className="text-4xl font-bold md:text-6xl">
-            Crafting Digital Experiences, Designing Tomorrow.
-          </h1>
-          {/* DESC */}
-          <p className="md:text-xl">
-            Welcome to my digital canvas, where innovation and creativity
-            converge. With a keen eye for aesthetics and a mastery of code, my
-            portfolio showcases a diverse collection of projects that reflect my
-            commitment to excellence.
-          </p>
-          {/* BUTTONS */}
-          <div className="flex w-full gap-4">
-            <button className="rounded-lg bg-black p-4 text-white ring-1 ring-black">
-              View My Work
-            </button>
-            <button className="rounded-lg p-4 ring-1 ring-black">
-              Contact Me
-            </button>
-          </div>
-        </div>
-      </div>
-    </motion.div>
-  );
-};
+      <HeroImage />
 
-export default Homepage;
+      <HeroContext />
+    </motion.main>
+  );
+}
