96a8305376a5c0dabd2736a8efc6a8f9281eec7b
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-07-05 11:58:04 +0500

    portfolio idea dropped partially.

--- src/components/SectionTitle.tsx
+++
@@ -1,4 +0,0 @@
-// Component used for section titles all over this portfolio.
-export default function SectionTitle({ children }: ChildrenProp) {
-  return <h4 className="text-3xl font-bold text-black/75">{children}</h4>;
-}
--- types.d.ts
+++
@@ -1,1 +0,0 @@
-type ChildrenProp = { children?: Readonly<React.ReactNode> };
