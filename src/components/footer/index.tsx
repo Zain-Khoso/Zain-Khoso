9e9762ff7cd56928d06f6343a41028d4773ac063
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-10-18 16:25:59 +0500

    fix: Accessibility

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -39,22 +39,26 @@ export default function Footer() {
           </P>
 
           <div className="mt-8 flex items-center gap-4 md:gap-8">
-            <Link href="https://linkedin.com/in/zain-khoso" target="_blank">
+            <Link href="https://linkedin.com/in/zain-khoso" target="_blank" aria-label="LinkedIn">
               <FaLinkedinIn className="fill-primary/65 hover:fill-primary transition" size={24} />
             </Link>
 
-            <Link href="https://github.com/Zain-Khoso" target="_blank">
+            <Link href="https://github.com/Zain-Khoso" target="_blank" aria-label="Github">
               <FaGithub className="fill-primary/65 hover:fill-primary transition" size={24} />
             </Link>
 
-            <Link href="https://www.fiverr.com/zainkhosodev" target="_blank">
+            <Link href="https://www.fiverr.com/zainkhosodev" target="_blank" aria-label="Fiverr">
               <TbBrandFiverr
                 className="stroke-primary/65 hover:stroke-primary fill-none transition"
                 size={24}
               />
             </Link>
 
-            <Link href="https://www.upwork.com/freelancers/~0149087552e102ee7e" target="_blank">
+            <Link
+              href="https://www.upwork.com/freelancers/~0149087552e102ee7e"
+              target="_blank"
+              aria-label="Upworks"
+            >
               <TbBrandUpwork
                 className="stroke-primary/65 hover:stroke-primary fill-none transition"
                 size={24}
